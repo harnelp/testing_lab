a854861cb49af6f80d4e9bdd9beb69294c9529af
Harnel <harnelp@gmail.com>
2024-02-10 13:56:00 -0500

    test solution

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ def sum(a, b):
 
 
 class SumTest(unittest.TestCase):
-    def test_sum(self):
-        self.assertEqual(sum(1, 2), 5)
+    def test_sum(self):            #5 
+        self.assertEqual(sum(1, 2), 3) # el test falla porque indirectamente, la suma entre (1,2) se iguala a 5
+                                    # si hacemos el cambio para el 3, el test pasa sin problema
 
 
 if __name__ == '__main__':
